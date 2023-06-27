89e85c7804e8d88e9fde3f6fa9b2297a515240af
JeanCSinisterra <84473537+JeanCSinisterra@users.noreply.github.com>
2023-06-27 06:49:49 -0500

    reverse String exercise done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    return str.split("").reverse().join("");
 };
+
+console.log(reverseString('hello there'));
+console.log(reverseString("My name is Sidney 43212, Wallaby City!"));
 
 // Do not edit below this line
 module.exports = reverseString;
