b1eace7f76f41f49e37d6fb4d40f9c2e46c52e79
JeanCSinisterra <84473537+JeanCSinisterra@users.noreply.github.com>
2023-06-27 06:37:54 -0500

    repeatString exercise done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,23 @@
-const repeatString = function() {
+const repeatString = function (str, count) {
+    // if (count < 0 ) {
+    //     return 'Count must be greater than or equal to 0';
+    // }
 
+    let result = '';
+    for (let i = 0; i < count; i++) {
+        result += str;
+    }
+    return result;
 };
+
+const number = Math.floor(Math.random() * 1000);
+
+console.log(repeatString('hey', 3));
+console.log(repeatString('hey', 10));
+console.log(repeatString('hey', 1));
+console.log(repeatString('hey', 0));
+console.log(repeatString('hey', -1));
+console.log(repeatString('hey', number));
 
 // Do not edit below this line
 module.exports = repeatString;
