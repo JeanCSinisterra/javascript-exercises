238d5a5cec6a94ef52c251f1bb48aef4992cdbcb
JeanCSinisterra <84473537+JeanCSinisterra@users.noreply.github.com>
2023-06-29 06:44:19 -0500

    removeFromArray exercise done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args ) {
+    return array.filter(element => !args.includes(element));
 };
+
+let myArray = ([1, 2, 3, 4]);
+// You have to call your function as the New Array 
+let newArray = removeFromArray(myArray,3);
+
+// Print the result
+console.log(newArray);
 
 // Do not edit below this line
 module.exports = removeFromArray;
