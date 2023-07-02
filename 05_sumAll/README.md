382e38e2ed66c1344ae32a47cef918a4b36e98f6
JeanCSinisterra <84473537+JeanCSinisterra@users.noreply.github.com>
2023-07-02 15:48:07 -0500

    solution to sumAll problem

--- 05_sumAll/README.md
+++ 05_sumAll/README.md
@@ -1,10 +1,10 @@
 # Exercise 05 - sumAll
 
-Implement a function that takes 2 integers and returns the sum of every number between(and including) them:
+    Implement a function that takes 2 integers and returns the sum of every number between(and including) them:
 
-```javascript
-sumAll(1, 4) // returns the sum of 1 + 2 + 3 + 4 which is 10
-```
+    ```javascript
+    sumAll(1, 4) // returns the sum of 1 + 2 + 3 + 4 which is 10
+    ```
 
 
 ## Hints
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,9 +1,30 @@
-const sumAll = function() {
-    
+function sumAll(num1, num2) {
+    // Check if both arguments are numbers, return 'ERROR' if not
+    if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+        return 'ERROR';
+    }
 
-};
+    // Check if both numbers are non-negative, return 'ERROR' if not
+    if (num1 < 0 || num2 < 0) {
+        return 'ERROR';
+    }
 
-console.log(sumAll(1, 4));
+    // Determine which number is smaller and which is larger
+    let min = Math.min(num1, num2);
+    let max = Math.max(num1, num2);
+
+    // Initialize a variable to hold the sum
+    let sum = 0;
+
+    // Loop from the smaller number to the larger number,
+    // adding each number to the sum
+    for (let i = min; i <= max; i++) {
+        sum += i;
+    }
+
+    // Return the sum
+    return sum;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
