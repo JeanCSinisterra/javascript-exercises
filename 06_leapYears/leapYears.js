4934a681f724eaf29e7cf706da423be8e7083b2b
JeanCSinisterra <84473537+JeanCSinisterra@users.noreply.github.com>
2023-07-02 16:26:16 -0500

    leapYear problem solved

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,19 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    // Check if the year is divisible by 4
+    if (year %4 !== 0) {
+        return false
+    } else if (year % 100 !== 0) {
+        return true
+    } else if (year % 400 !== 0) {
+        return false
+    } else {
+        return true
+    }
+    // Another solution but with the ternary method:
+    // return ( year % 4 === 0 && year % 100 !== 0 ) || ( year % 400 === 0)
 };
+console.log(leapYears(2000));
+console.log(leapYears(1985));
 
 // Do not edit below this line
 module.exports = leapYears;
