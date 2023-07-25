d54a969329a60d0cf897b6b6fd31224be2df4e81
JeanCSinisterra <84473537+JeanCSinisterra@users.noreply.github.com>
2023-07-25 06:30:22 -0500

    palindromes function solutioned

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -2,7 +2,11 @@ const palindromes = function (str) {
     let newStr = str.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
     let reverseStr = newStr.split("").reverse().join("");
     return newStr === reverseStr;
-} ;
+};
+palindromes("racecar");
+palindromes("Racecar");
+palindromes("Race car");
+palindromes("Race car, I'm a racecar.");
 
 // Do not edit below this line
 module.exports = palindromes;
