5a1752c22c89b42cccb566e856944df756f74b1a
JeanCSinisterra <84473537+JeanCSinisterra@users.noreply.github.com>
2023-07-25 06:49:24 -0500

    fibonacci solution entered

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function (number) {
+    // Function only must have numbers
+    if (!Number.isInteger(number)) {
+        throw new Error("Input must be a number");
+    }
+    // Function must not handle negative numbers
+    if (number < 0 ) {
+        throw new Error("Input must be a positive integer");
+    }
+    let num1 = 0;
+    let num2 = 1;
+    for (let i = 0; i < number; i++) {
+        let temp = num1;
+        num1 = num2;
+        num2 = temp + num2;
+    }
+    return num1;
 };
 
 // Do not edit below this line
