24d880aee437767691767441e3431344f3357482
JeanCSinisterra <84473537+JeanCSinisterra@users.noreply.github.com>
2023-07-25 06:16:27 -0500

    calculator solutions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,65 @@
-const add = function() {
-	
+const add = function(a, b) {
+  // Validate if the inputs are numbers
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    throw new Error('Inputs must be numbers');
+  }
+  let result = a + b;
+  return result;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  // Validate if the inputs are numbers
+   if (typeof a !== 'number' || typeof b !== 'number') {
+    throw new Error('Inputs must be numbers');
+  }
+  let result = a - b
+  return result;
 };
 
-const sum = function() {
-	
+
+const sum = function(numbers) {
+  // Validate if the inputs is an array
+  if (!Array.isArray(numbers)) {
+    throw new Error('Input is not an array');
+  }
+  // Must be validate that the array is not empty
+   if (numbers.length === 0) {
+    return 0;
+  }
+	let total = 0;
+  for (let num of numbers) {
+    total += num;
+  }
+  return total
 };
 
-const multiply = function() {
-
+const multiply = function(a, b) {
+  // Validate if the inputs are numbers
+   if (typeof a !== 'number' || typeof b !== 'number') {
+    throw new Error('Inputs must be numbers');
+  }
+  let result = a * b
+  return result;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	// Validate the inputs are numbers
+   if (typeof a !== 'number' || typeof b !== 'number') {
+    throw new Error('Inputs must be numbers');
+  }
+  return Math.pow(a,b)
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	// Validate if the inputs are positive integers
+  if (typeof n !== 'number' || n < 0 || !Number.isInteger(n)) {
+    throw new Error('Input must be a positive integer');
+  }
+  if (n === 0 ) {
+    return 1;
+  } else {
+  return n * factorial(n - 1);
+  }
 };
 
 // Do not edit below this line
