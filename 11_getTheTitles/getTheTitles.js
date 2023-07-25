21d28f46f3e2fe9820b305c81c9933a551507866
Jean Carlo Sinisterra <84473537+JeanCSinisterra@users.noreply.github.com>
2023-07-25 10:31:55 -0500

    new array of Books titles entered

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -17,12 +17,44 @@ const getTheTitles = function(arr) {
 
 const books = [
     {
-      title: 'Book',
-      author: 'Name'
+        title: 'The Great Gatsby',
+        author: 'F. Scott Fitzgerald'
     },
     {
-      title: 'Book2',
-      author: 'Name2'
+        title: 'To Kill a Mockingbird',
+        author: 'Harper Lee'
+    },
+    {
+        title: '1984',
+        author: 'George Orwell'
+    },
+    {
+        title: 'Pride and Prejudice',
+        author: 'Jane Austen'
+    },
+    {
+        title: 'The Lord of the Rings',
+        author: 'J.R.R. Tolkien'
+    },
+    {
+        title: 'Harry Potter and the Sorcerer\'s Stone',
+        author: 'J.K. Rowling'
+    },
+    {
+        title: 'The Catcher in the Rye',
+        author: 'J.D. Salinger'
+    },
+    {
+        title: 'Brave New World',
+        author: 'Aldous Huxley'
+    },
+    {
+        title: 'Crime and Punishment',
+        author: 'Fyodor Dostoevsky'
+    },
+    {
+        title: 'The Chronicles of Narnia',
+        author: 'C.S. Lewis'
     }
 ];
 
