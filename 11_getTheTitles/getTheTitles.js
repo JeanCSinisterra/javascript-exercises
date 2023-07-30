0c8dbecf471dbc792d2d02d337b0b1b4d55c530f
JeanCSinisterra <84473537+JeanCSinisterra@users.noreply.github.com>
2023-07-30 18:32:51 -0500

    getTheTitles solution entered

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,4 +1,16 @@
 const getTheTitles = function() {
+    // Check if the input is an Array
+    if (!Array.isArray(arr)) {
+        throw new Error("Input must be an Array.");
+    }
+
+    // Extract the titles of the books in the array
+    const bookTitles = arr.map((obj) => {
+        if (!("title" in obj)) {
+            throw new Error("Each element in the array must be an object with a 'title' property.");
+        }
+        return obj.title;
+    });
 
     return bookTitles;
 };
@@ -49,51 +61,5 @@ const books = [
 const booksTitles = getTheTitles(books);
 console.log(booksTitles);
 
-
-const books = [
-    {
-        title: 'The Great Gatsby',
-        author: 'F. Scott Fitzgerald'
-    },
-    {
-        title: 'To Kill a Mockingbird',
-        author: 'Harper Lee'
-    },
-    {
-        title: '1984',
-        author: 'George Orwell'
-    },
-    {
-        title: 'Pride and Prejudice',
-        author: 'Jane Austen'
-    },
-    {
-        title: 'The Lord of the Rings',
-        author: 'J.R.R. Tolkien'
-    },
-    {
-        title: 'Harry Potter and the Sorcerer\'s Stone',
-        author: 'J.K. Rowling'
-    },
-    {
-        title: 'The Catcher in the Rye',
-        author: 'J.D. Salinger'
-    },
-    {
-        title: 'Brave New World',
-        author: 'Aldous Huxley'
-    },
-    {
-        title: 'Crime and Punishment',
-        author: 'Fyodor Dostoevsky'
-    },
-    {
-        title: 'The Chronicles of Narnia',
-        author: 'C.S. Lewis'
-    }
-];
-
-const booksTitles = getTheTitles(books);
-console.log(booksTitles);
 // Do not edit below this line
 module.exports = getTheTitles;
